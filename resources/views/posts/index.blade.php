3138f4e227a421d88401ed3e97146c8ee13b1977
JamesTong <jameskai17@gmail.com>
2018-10-24 00:07:53 +0800

    使用共同layout

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,12 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Blog</title>
-</head>
-<body>
+@extends('layouts.master')
+
+@section('title')
+  Blog
+@endsection
+
+@section('content')
   <h1>所有文章</h1>
   <table>
   <thead>
@@ -76,5 +74,4 @@
       @endforeach
     </tbody>
   </table>
-</body>
-</html>
+@endsection
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,18 +1,15 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>{{ $post->title }} | Blog</title>
-</head>
-<body>
+@extends('layouts.master')
+
+@section('title')
+  {{ $post->title }} | Blog
+@endsection
+
+@section('content')
   <h1>{{ $post->title }}</h1>
   <div class="post-info">
     <span>作者: {{ $post->author }}</span>
     <span>日期: {{ $post->created_at }}</span>
   </div>
   <div class="content">{{ $post->body }}</div>
-  
-</body>
-</html>
+@endsection
