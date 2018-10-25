cbf5b9717994b583e47f8f220913e32dd9c04359
JamesTong <jameskai17@gmail.com>
2018-10-25 14:57:24 +0800

    修正 bug

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,8 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', 'PostsController@index')->name('posts_index');
-Route::get('/posts/{post}', 'PostsController@show')->name('posts_show');
 Route::get('/post/create', 'PostsController@create')->name('posts_create');
+Route::get('/post/{post}', 'PostsController@show')->name('posts_show')->where('post', '[0-9]+');
 Route::post('/posts', 'PostsController@store')->name('posts_store');
 Route::get('/post/{post}/edit', 'PostsController@edit')->name('posts_edit');
 Route::PUT('/post/{post}', 'PostsController@update')->name('posts_update');
