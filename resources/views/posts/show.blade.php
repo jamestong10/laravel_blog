bf42cba03da299145571593323c7adc5eebbf88d
JamesTong <jameskai17@gmail.com>
2018-10-25 15:28:17 +0800

    調整顯示規則

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,7 +8,7 @@
   <h1>{{ $post->title }}</h1>
   <div class="post-info">
     <span>作者: {{ $post->author }}</span>
-    <span>日期: {{ \Carbon\Carbon::parse($post->created_at)->format('Y/m/d G:i:s') }}</span>
+    <span>日期: {{ $post->created_at->format('Y/m/d G:i:s') }}</span>
   </div>
   <div class="content">{{ $post->body }}</div>
   <a href="{{route('posts_index')}}" class="btn btn-primary">Back</a>
