761c58506fc2cb71f78bfd533869cb43ef46c126
JamesTong <jameskai17@gmail.com>
2018-10-26 14:34:17 +0800

    新增post 驗證失敗 test case

--- tests/Feature/PostsControllerTest.php
+++ tests/Feature/PostsControllerTest.php
@@ -11,21 +11,26 @@ class PostsControllerTest extends TestCase
     public function testCreateNewPost() {
 
         $post  = Factory('App\Post')->make();
-        
         $response = $this->post(route('posts_store'), $post->toArray());
         
         $response->assertStatus(302);
         $response->assertRedirect(route('posts_index'));
-
+        
         $insertedPost = Post::latest()->first();
         $this->assertEquals(
-            [
-                "title" => $insertedPost->title,
-                "body" => $insertedPost->body 
-            ], 
-            [
-                "title" => $post->title,
-                "body" => $post->body 
-            ]);
+            [ $insertedPost->title,$insertedPost->body ], 
+            [ $post->title, $post->body ]);
+    }
+
+    public function testPostValidation() {
+        $post = new Post;
+        $post->title = '';
+        $post->body  = '';
+
+        $response = $this->post(route('posts_store'), $post->toArray());
+        $response->assertSessionHasErrors();
+        $errors = session('errors');
+        $this->assertEquals($errors->get('title')[0],"The title field is required.");
+        $this->assertEquals($errors->get('body')[0],"The body field is required.");
     }
 }
